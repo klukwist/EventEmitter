dcf5bc8a28d0045ce07ef019f08402373ce54a8e
Alexey Che <info@klukwa.com>
2019-11-12 10:07:05 +0300

    Update and rename MyFragment to MyFragment.kt

---
+++ MyFragment.kt
@@ -0,0 +1,15 @@
+class MyFragment : Fragment() {
+
+    private val navigationEventsObserver = NavigationEvent.createObserver { event ->
+            when (event) {
+                is PromoNavigation.ShowCategoriesList -> promoViewModel.showCategoriesList()
+                is PromoNavigation.PlayVideo -> videoPlayerView.loadUrl(event.url)
+                is PromoNavigation.OpenProduct -> openProduct(event.productId, event.otherInfo)
+            }
+        }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
+            myViewModel.navigator.emitter.observe(viewLifecycleOwner, navigationEventsObserver)
+        }
+}
