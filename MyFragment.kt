2630ddfe33556c552e5ddc5fe4cea25f1ba2409d
Alexey Che <info@klukwa.com>
2019-12-11 14:21:41 +0300

    Update MyFragment.kt

--- MyFragment.kt
+++ MyFragment.kt
@@ -1,18 +1,20 @@
 class MyFragment : Fragment() {
     private val navigationEventsObserver = Events.EventObserver { event ->
             when (event) {
-                is MyFragmentNavigation.ShowCameraPermission -> showCameraPermission()
+                is MyFragmentNavigation.ShowCategoryList -> ShowCategoryList()
                 is MyFragmentNavigation.PlayVideo -> videoPlayerView.loadUrl(event.url)
-                is MyFragmentNavigation.OpenProduct -> openProduct(event.productId, event.otherInfo)
+                is MyFragmentNavigation.OpenProduct -> openProduct(id = event.productId, name = event.otherInfo)
             }
         }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
             super.onViewCreated(view, savedInstanceState)
             myViewModel.navigator.emitter.observe(viewLifecycleOwner, navigationEventsObserver)
+            myViewModelSecond.navigator.emitter.observe(viewLifecycleOwner, navigationEventsObserver)
+            myViewModelThird.navigator.emitter.observe(viewLifecycleOwner, navigationEventsObserver)
         }
     
-    private fun showCameraPermission(){
+    private fun ShowCategoryList(){
         ...
     }
     
